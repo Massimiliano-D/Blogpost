3daea3ef3d92a264ccf04eb06469c587eee83fc1
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-11-07 15:08:58 +0100

    Initial commit

---
+++ src/test/java/Posting/Blogpost/BlogpostApplicationTests.java
@@ -0,0 +1,13 @@
+package Posting.Blogpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
